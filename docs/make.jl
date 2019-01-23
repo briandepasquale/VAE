5860791d1ee806f9af6352ad4fa5ca0d330bc65a
Brian DePasquale <depasquale@princeton.edu>
2019-01-22 22:17:43 -0500

    try again

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ push!(LOAD_PATH,"../src/")
 
 using Documenter, VAE
 
-makedocs(sitename="VAE.jl",modules=[VAE], doctest=false)
+makedocs(sitename="VAE", modules=[VAE], doctest=false)
  
 deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
            repo = "github.com/briandepasquale/VAE.git")
