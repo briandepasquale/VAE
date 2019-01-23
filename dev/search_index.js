1cd0abfb59b97d7989e2b10f79c81c1fa5a58ca4
autodocs <autodocs>
2019-01-23 01:32:54 +0000

    build based on 234f3d3

---
+++ dev/search_index.js
@@ -0,0 +1,115 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "VAE.jl Documentation",
+    "title": "VAE.jl Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#VAE.jl-Documentation-1",
+    "page": "VAE.jl Documentation",
+    "title": "VAE.jl Documentation",
+    "category": "section",
+    "text": "This is a package for implementing VAEs in julia."
+},
+
+{
+    "location": "#My-subtitle-1",
+    "page": "VAE.jl Documentation",
+    "title": "My subtitle",
+    "category": "section",
+    "text": "More text"
+},
+
+{
+    "location": "#Tutorials-1",
+    "page": "VAE.jl Documentation",
+    "title": "Tutorials",
+    "category": "section",
+    "text": "Pages = [\n    \"tutorials/page1.md\",\n    \"tutorials/page2.md\"\n    ]\nDepth = 2"
+},
+
+{
+    "location": "#Another-Section-1",
+    "page": "VAE.jl Documentation",
+    "title": "Another Section",
+    "category": "section",
+    "text": "Pages = [\n    \"sec2/page1.md\",\n    \"sec2/page2.md\"\n    ]\nDepth = 2"
+},
+
+{
+    "location": "#Index-1",
+    "page": "VAE.jl Documentation",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "sec2/page1/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "#TitleSome text describing this section"
+},
+
+{
+    "location": "sec2/page1/#Subtitle-1",
+    "page": "-",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "@docs VAE.transform(vae,X)``"
+},
+
+{
+    "location": "sec2/page2/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "#TitleSome text describing this section"
+},
+
+{
+    "location": "sec2/page2/#Subtitle-1",
+    "page": "-",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "@docs VAE.transform(vae,X)``"
+},
+
+{
+    "location": "tutorial/page1/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "#TitleSome text describing this section"
+},
+
+{
+    "location": "tutorial/page1/#Subtitle-1",
+    "page": "-",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "@docs VAE.transform(vae,X)``"
+},
+
+{
+    "location": "tutorial/page2/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "#TitleSome text describing this section"
+},
+
+{
+    "location": "tutorial/page2/#Subtitle-1",
+    "page": "-",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "@docs VAE.transform(vae,X)``"
+},
+
+]}
