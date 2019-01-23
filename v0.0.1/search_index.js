6f377c4de5f0bd28188fe9e45f610e4278b0dce4
autodocs <autodocs>
2019-01-23 05:04:28 +0000

    build based on f342baa

---
+++ v0.0.1/search_index.js
@@ -0,0 +1,107 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "VAE.jl Documentation",
+    "title": "VAE.jl Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#VAE.jl-Documentation-1",
+    "page": "VAE.jl Documentation",
+    "title": "VAE.jl Documentation",
+    "category": "section",
+    "text": "This is a package for implementing VAEs in julia."
+},
+
+{
+    "location": "#VAE.transform-Tuple{Any,Any}",
+    "page": "VAE.jl Documentation",
+    "title": "VAE.transform",
+    "category": "method",
+    "text": "transform(vae,X)\n\nTransform data by mapping it into the latent space\n\n\n\n\n\n"
+},
+
+{
+    "location": "#Functions-1",
+    "page": "VAE.jl Documentation",
+    "title": "Functions",
+    "category": "section",
+    "text": "VAE.transform(vae,X)"
+},
+
+{
+    "location": "#Index-1",
+    "page": "VAE.jl Documentation",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "sec2/page1/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "#TitleSome text describing this section"
+},
+
+{
+    "location": "sec2/page1/#Subtitle-1",
+    "page": "-",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "@docs VAE.transform(vae,X)``"
+},
+
+{
+    "location": "sec2/page2/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "#TitleSome text describing this section"
+},
+
+{
+    "location": "sec2/page2/#Subtitle-1",
+    "page": "-",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "@docs VAE.transform(vae,X)``"
+},
+
+{
+    "location": "tutorial/page1/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "#TitleSome text describing this section"
+},
+
+{
+    "location": "tutorial/page1/#Subtitle-1",
+    "page": "-",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "@docs VAE.transform(vae,X)``"
+},
+
+{
+    "location": "tutorial/page2/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "#TitleSome text describing this section"
+},
+
+{
+    "location": "tutorial/page2/#Subtitle-1",
+    "page": "-",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "@docs VAE.transform(vae,X)``"
+},
+
+]}
